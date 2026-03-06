dcf84153ba74cd64d7e22de7fd77bbcdea0e4230
momo226-code <nanaidriss70@gmail.com>
2026-03-06 23:37:21 +0000

    connect ngrok backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://jensen-unadoring-realizingly.ngrok-free.dev/api",
 });
 
 API.interceptors.request.use((config) => {
